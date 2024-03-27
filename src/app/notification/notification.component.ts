3cc26f30e978fd11407853bfc864f54abba2a7a5
Nudthaya Khemcharoen <nudthaya@sec.or.th>
2024-03-27 15:25:06 +0700

    notification

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faBookBookmark, faChartLine, faCircleExclamation, faGraduationCap, faUser, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-notification',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent {
-
+  faXmark = faXmark;
+  faInfo = faUser;
+  faNoti = faCircleExclamation;
+  faEnrollment = faGraduationCap;
+  faThesis = faBookBookmark;
+  faDashboard = faChartLine;
 }
