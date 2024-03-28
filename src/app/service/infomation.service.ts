8467b45118df1dacaaace41f74b66abac6bc6082
Nudthaya Khemcharoen <nudthaya@sec.or.th>
2024-03-28 14:47:56 +0700

    แก้ ดึงข้อมูล Info

--- src/app/service/infomation.service.ts
+++ src/app/service/infomation.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Student } from '../models/student';
+import { UserInfo } from '../models/user-info';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class InfomationService {
 
   constructor(private http: HttpClient) { }
 
-  getInformation(userName: string): Observable<Student> {
-    return this.http.get<Student>(`http://localhost:5043/Information?userName=${userName}`);
+  getInformation(userName: string): Observable<UserInfo> {
+    return this.http.get<UserInfo>(`http://localhost:5043/Information/${userName}`);
   }
 }
--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faBookBookmark, faChartLine, faCircleExclamation, faGraduationCap, faUser, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { InfomationService } from '../service/infomation.service';
 import { Student } from '../models/student';
+import { UserInfo } from '../models/user-info';
 
 @Component({
   selector: 'app-user-info',
@@ -16,15 +17,21 @@ export class UserInfoComponent implements OnInit{
   faThesis = faBookBookmark;
   faDashboard = faChartLine;
 
-  userInfo = {} as Student;
+  userInfo = {} as UserInfo;
+  student = {} as Student;
+  state = "student";
   constructor(
     private informationService: InfomationService
   )
   {}
 
   ngOnInit(): void {
-    this.informationService.getInformation("student").subscribe( s => {
+    this.informationService.getInformation(this.state).subscribe( s => {
       this.userInfo = s;
-    })
+    });
+
+    if (this.state = "student") {
+
+    }
   }
 }
