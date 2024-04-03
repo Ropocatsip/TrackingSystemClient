372ab7e83d668bf5b69b65035f5301f102e1b5d6
Nudthaya Khemcharoen <nudthaya@sec.or.th>
2024-04-03 10:24:55 +0700

    เพิ่มการดึงข้อมูล notification

--- src/app/models/notification.ts
+++ src/app/models/notification.ts
@@ -2,5 +2,6 @@ export class Notification {
     userName: string = "";
     type: string = "";
     detail: string = "";
+    sendDate: string = "";
     isClose: boolean = false;
 }
--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
-import { faBookBookmark, faChartLine, faCircleExclamation, faExchange, faGraduationCap, faUser, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { LocalStorageService } from '../service/local-storage.service';
+import { NotificationService } from '../service/notification.service';
+import { Notification } from '../models/notification';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.css']
 })
-export class NotificationComponent {
+export class NotificationComponent implements OnInit {
   faXmark = faXmark;
-  faInfo = faUser;
-  faNoti = faCircleExclamation;
-  faEnrollment = faGraduationCap;
-  faThesis = faBookBookmark;
-  faDashboard = faChartLine;
-  faLogOut = faExchange;
+
+  notifications = [] as Notification[];
+
+  constructor(
+    private localStorageService: LocalStorageService,
+    private notificationService: NotificationService
+  ) {}
+
+  ngOnInit(): void {
+    const userName = this.localStorageService.getItem('userName');
+    if (userName) {
+      this.notificationService.getNotifications(userName).subscribe({
+        next: (result) => {
+          this.notifications = result;
+        },
+        error: (er) => {
+          alert(er.error);
+        },
+        complete: () => {
+        }
+      });
+    }
+  }
 }
